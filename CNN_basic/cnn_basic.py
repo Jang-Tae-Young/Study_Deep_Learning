73331367fc405fa185c2abd7552601851fe12936
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-09 21:39:51 +0900

    update

--- CNN_basic/cnn_basic.py
+++ CNN_basic/cnn_basic.py
@@ -1,3 +1,21 @@
 import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 
 print(tf.__version__)
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+train_images = train_images.reshape((60000,28,28,1))
+test_images = test_images.reshape((10000,28,28,1))
+
+train_images, test_images = train_images/255.0, test_images/255.0
+
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+
+model.summary()
+
