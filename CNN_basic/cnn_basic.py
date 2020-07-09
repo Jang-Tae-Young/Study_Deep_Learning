3d4dab2cd16c4dabb015b6ec888ffdf18fcb1bec
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-09 22:20:57 +0900

    Update cnn_basic.py

--- CNN_basic/cnn_basic.py
+++ CNN_basic/cnn_basic.py
@@ -17,5 +17,17 @@ model.add(layers.Conv2D(64,(3,3),activation='relu'))
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3),activation='relu'))
 
-model.summary()
+# model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
