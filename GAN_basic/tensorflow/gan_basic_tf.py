e019e4ce942070efbe5e81cec8a052d550121f29
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-17 14:07:05 +0900

    update

---
+++ GAN_basic/tensorflow/gan_basic_tf.py
@@ -0,0 +1,166 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers, datasets
+import time
+import gan_sub_functions as gsf
+from IPython import display
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # 이미지를 [-1, 1]로 정규화합니다.
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+# 데이터 배치를 만들고 섞습니다.
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+generator = gsf.make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+
+plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+
+discriminator = gsf.make_discriminator_model()
+decision = discriminator(generated_image)
+print (decision)
+
+generator_optimizer     = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+
+
+EPOCHS = 50
+gsf.train(train_dataset, generator, discriminator, EPOCHS, BATCH_SIZE, generator_optimizer, discriminator_optimizer)
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+
+gsf.train(train_dataset, generator, discriminator, EPOCHS, BATCH_SIZE, generator_optimizer, discriminator_optimizer)
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+
+gsf.display_image(EPOCHS)
+
+
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    last = -1
+    for i,filename in enumerate(filenames):
+        frame = 2*(i**0.5)
+        if round(frame) > round(last):
+        last = frame
+        else:
+        continue
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
+# import IPython
+if IPython.version_info > (6,2,0,''):
+  display.Image(filename=anim_file)
+
+# from __future__ import absolute_import, division, print_function, unicode_literals
+# import tensorflow as tf
+# import glob
+# import imageio
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import os
+# import PIL
+# from tensorflow.keras import layers, datasets
+# import time
+# from IPython import display
+
+
+
+
+# def make_generator_model():
+#     model = tf.keras.Sequential()
+#     model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.LeakyReLU())
+
+#     model.add(layers.Reshape((7, 7, 256)))
+#     assert model.output_shape == (None, 7, 7, 256) # 주목: 배치사이즈로 None이 주어집니다.
+
+#     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+#     assert model.output_shape == (None, 7, 7, 128)
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.LeakyReLU())
+
+#     model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+#     assert model.output_shape == (None, 14, 14, 64)
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.LeakyReLU())
+
+#     model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+#     assert model.output_shape == (None, 28, 28, 1)
+
+#     return model
+
+# def make_discriminator_model():
+#     model = tf.keras.Sequential()
+#     model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+#                                      input_shape=[28, 28, 1]))
+#     model.add(layers.LeakyReLU())
+#     model.add(layers.Dropout(0.3))
+
+#     model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+#     model.add(layers.LeakyReLU())
+#     model.add(layers.Dropout(0.3))
+
+#     model.add(layers.Flatten())
+#     model.add(layers.Dense(1))
+
+#     return model
+
+
+
+# (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+# train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+# train_images = (train_images - 127.5) / 127.5 # 이미지를 [-1, 1]로 정규화합니다.
+
+# BUFFER_SIZE = 60000
+# BATCH_SIZE = 256
+
+# # 데이터 배치를 만들고 섞습니다.
+# train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+# generator = make_generator_model()
+
+# noise = tf.random.normal([1, 100])
+# generated_image = generator(noise, training=False)
+
+# plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+# # plt.show()
+
+# discriminator = make_discriminator_model()
+# decision = discriminator(generated_image)
+# print (decision)
+
+
+
+
