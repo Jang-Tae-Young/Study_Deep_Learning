42e26b5bf4315348a0ac70d834249334d2f5579e
Jang-Tae-Young <tyjang1020@gmail.com>
2020-07-17 14:08:22 +0900

    Update gan_basic_tf.py

--- GAN_basic/tensorflow/gan_basic_tf.py
+++ GAN_basic/tensorflow/gan_basic_tf.py
@@ -69,9 +69,9 @@ with imageio.get_writer(anim_file, mode='I') as writer:
     for i,filename in enumerate(filenames):
         frame = 2*(i**0.5)
         if round(frame) > round(last):
-        last = frame
+            last = frame
         else:
-        continue
+            continue
         image = imageio.imread(filename)
         writer.append_data(image)
     image = imageio.imread(filename)
